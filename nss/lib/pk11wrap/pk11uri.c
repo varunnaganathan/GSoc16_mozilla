1b6eae82288832b456859ba679be97a2da05503d
varunnaganathan <varunnaganathan912@gmail.com>
2016-05-25 14:14:52 +0530

    added the findmodulebyuri function

--- nss/lib/pk11wrap/pk11uri.c
+++ nss/lib/pk11wrap/pk11uri.c
@@ -48,3 +48,49 @@ PK11_GetTokenUri(PK11SlotInfo *slot)
     p11_kit_uri_free(uri);
     return result;
 }
+
+//Similar to SECMOD_FindModule
+SECMODModule *PK11_FindModuleByUri(char *uri)
+{
+    P11KitUri *URI = NULL;
+    CK_ATTRIBUTE_POINTER library_descripton;
+    CK_ATTRIBUTE_POINTER library_manufacturer;
+    CK_ATTRIBUTE_POINTER library_version;
+    SECMODModuleList *listnode;
+    SECMODModule *module = NULL;
+    CK_INFO *moduleinfo;
+    SECStatus status;
+    int st = p11_kit_uri_parse(uri, P11_KIT_URI_FOR_MODULE, URI);
+    if (!st)
+    {
+        //Raise error
+    }
+    /*Have doubts regarding the CK_ATTRIBUTE_TYPE.passing ints 0 for library-description
+    1 for libarry-manufacturer and 2 for library-version */
+    library_description = p11_kit_uri_get_attribute(URI, 0);
+    library_manufacturer = p11_kit_uri_get_attribute(URI, 1);
+    library_version = p11_kit_uri_get_attribute(URI, 2);
+    //Ask what this means.Copied because was present in SECMOD_FindModule
+    if (!moduleLock) {
+        PORT_SetError(SEC_ERROR_NOT_INITIALIZED);
+        return module;
+    }
+    SECMOD_GetReadLock(moduleLock);
+    for(listnode =  modules; listnode != NULL; listnode = listnode->next)
+    {
+        status = PK11_GetModInfo(&listnode->module, moduleinfo);
+        //use PORT_Strcmp to compare
+        if(moduleinfo->manufacturerID == library_manufacturer && moduleinfo->libraryDescription == libraryDescription && moduleinfo->libraryVersion == libraryVersion)
+        {
+            module = &listnode->module;
+            break;
+        }
+    }
+    SECMOD_ReleaseReadLock(moduleLock);
+    if(!module)
+    {
+        return (SECMODModule *)NULL;
+    }
+    return module;
+}
+
